885a855f4f0ddabf7defc46f87d8a498ec0cb43d
Alex G <123711305+alexgmax@users.noreply.github.com>
2025-12-03 12:51:06 +0700

    Create Package.swift (for iOS SPM)

---
+++ Package.swift
@@ -0,0 +1,21 @@
+import PackageDescription
+
+let package = Package(
+    name: "PremiumAdsAdapter",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "PremiumAdsAdapter",
+            targets: ["PremiumAdsAdapter"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "PremiumAdsAdapter",
+            url: "https://raw.githubusercontent.com/premium-ads/admob-adapter-ios-spm/refs/heads/main/PremiumAdsAdapter-2.2.6.xcframework.zip",
+            checksum: "606b4504219d73b7825d48f243f5c298dadf99e8f40dea9215b1bd5c93002cbb"
+        )
+    ]
+)
